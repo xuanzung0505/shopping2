78d03c83f0893fcb6f7a816925c8f52f4dd2eec2
xuanzung0505 <truongxuandung.0.5.0.5@gmail.com>
2022-11-04 17:35:36 +0700

    commit

--- draft/settings.py
+++ draft/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "core/static/"),
     os.path.join(BASE_DIR, "admin/static/"),
